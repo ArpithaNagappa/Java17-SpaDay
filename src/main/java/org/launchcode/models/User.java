1c1dfd67bd0b0eb4c7c5769407cb88b91714c3d7
Arpitha Nagappa <arpithagn021@gmail.com>
2024-01-25 20:11:19 -0600

    committing

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,31 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotNull(message = "Username is between 5 and 15 characters")
+    @Size(min=5,max=15,message = "Username is between 5 and 15 characters")
     private String username;
+    @NotNull
+    @Email
     private String email;
+    @Size(min=5,message = " password should have at least 6 characters long")
     private String password;
+    @Size(min=5,message = " password should have at least 6 characters long")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password,String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify=verify;
     }
 
     public String getUsername() {
@@ -36,8 +48,20 @@ public class User {
         return password;
     }
 
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    private void checkPassword(){
+
     }
 }
 
